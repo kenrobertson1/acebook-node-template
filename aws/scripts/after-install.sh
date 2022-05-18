34cc4f9c3b044230629659061ed6dac6e3d4393e
Khushnood Asif <khushnood99@gmail.com>
2022-05-18 12:05:27 +0100

    scripts update

---
+++ aws/scripts/after-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+
+# Copy war file from S3 bucket to tomcat webapp folder
+aws s3 cp s3://acebook-s3-template-${{ github.sha }}.zip /usr/local/tomcat9/webapps/acebook-s3-template-${{ github.sha }}.zip
+
+
+# Ensure the ownership permissions are correct.
+chown -R tomcat:tomcat /usr/local/tomcat9/webapps
