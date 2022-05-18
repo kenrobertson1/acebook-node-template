b54ebbe3edd496874d7fe3e2aec2ce416589b44d
Khushnood Asif <khushnood99@gmail.com>
2022-05-18 12:25:11 +0100

    scripts update 4

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://acebook-s3-template.zip /usr/local/tomcat9/webapps/acebook.zip
+aws s3 cp s3://acebook-s3-template.zip /usr/local/tomcat9/webapps/acebook-s3-template.zip
 
 
 # Ensure the ownership permissions are correct.
